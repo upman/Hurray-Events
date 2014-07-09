bce60402775eb4cfb5e5d4b835904fc80f8956bd
Ravi <upman16@gmail.com>
2014-07-10 01:13:17 +0530

    scrollspy working, landing section and social buttons done

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 require 'webrick'
 
-root = File.expand_path '~/hurray-events/tests'
+root = File.expand_path '~/hurray-events/hurray-events-website'
 server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
 
 trap 'INT' do server.shutdown end
