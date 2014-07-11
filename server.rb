dcad8d94d6186d27794232aeab2ac20dd9edc9e3
Ravi <upman16@gmail.com>
2014-07-11 20:04:07 +0530

    removed Commented out code(trying to do SCM right). Lorem ipsum for sections.

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 require 'webrick'
 
-root = File.expand_path '~/hurray-events/hurray-events-website'
+root = Dir.pwd
 server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
 
 trap 'INT' do server.shutdown end
